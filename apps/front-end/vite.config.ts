497b91f61d64eb337995b8216b361a3447851a2b
Cédric <cedric.hartland@gmail.com>
2025-09-16 13:45:54 +0200

    setup: connect front-back api proxy

--- apps/front-end/vite.config.ts
+++ apps/front-end/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: { 
+    alias: { 
+      '@': path.resolve(__dirname, './src')
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_BASE_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
